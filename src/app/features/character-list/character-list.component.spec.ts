fd5e9e705536f364eff9e577010dd973859783d8
cbcristhian <cristhian.castro@unosquare.com>
2024-08-26 22:18:01 -0400

    new unit test for favorite services

--- src/app/features/character-list/character-list.component.spec.ts
+++ src/app/features/character-list/character-list.component.spec.ts
@@ -8,24 +8,55 @@ import {
   StarWarsCharacter,
 } from '../../shared/interfaces/character.interface';
 import { Utils } from '../../shared/Utils';
+import { FavoriteService } from '../../services/favorite/favorite.service';
 
 describe('CharacterListComponent', () => {
   let component: CharacterListComponent;
   let fixture: ComponentFixture<CharacterListComponent>;
   let swapiService: jasmine.SpyObj<SwapiService>;
+  let favoriteService: jasmine.SpyObj<FavoriteService>;
+
+  const mockCharacter: StarWarsCharacter = {
+    name: 'Luke Skywalker',
+    height: '172',
+    mass: '77',
+    hair_color: 'blond',
+    skin_color: 'fair',
+    eye_color: 'blue',
+    birth_year: '19BBY',
+    gender: 'male',
+    homeworld: 'Tatooine',
+    films: [],
+    species: [],
+    vehicles: [],
+    starships: [],
+    created: new Date(),
+    edited: new Date(),
+    url: 'https://swapi.dev/api/people/1/',
+  };
 
   beforeEach(async () => {
     const swapiServiceSpy = jasmine.createSpyObj('SwapiService', [
       'getCharacters',
     ]);
+    const favoriteServiceSpy = jasmine.createSpyObj('FavoriteService', [
+      'isFavorite',
+      'addOrRemoveFavorite',
+    ]);
     await TestBed.configureTestingModule({
       declarations: [CharacterListComponent],
-      providers: [{ provide: SwapiService, useValue: swapiServiceSpy }],
+      providers: [
+        { provide: SwapiService, useValue: swapiServiceSpy },
+        { provide: FavoriteService, useValue: favoriteServiceSpy },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CharacterListComponent);
     component = fixture.componentInstance;
     swapiService = TestBed.inject(SwapiService) as jasmine.SpyObj<SwapiService>;
+    favoriteService = TestBed.inject(
+      FavoriteService
+    ) as jasmine.SpyObj<FavoriteService>;
 
     swapiService.getCharacters.and.returnValue(
       of({
@@ -60,93 +91,68 @@ describe('CharacterListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should create the component and load characters on init', () => {
-    expect(component).toBeTruthy();
+  it('should load characters on init', () => {
+    const mockResponse = {
+      count: 1,
+      next: 'next-url',
+      previous: 'previous-url',
+      results: [mockCharacter],
+    };
+    swapiService.getCharacters.and.returnValue(of(mockResponse));
 
     component.ngOnInit(); // Calls loadCharacters internally
 
     expect(component.characterList.length).toBe(1);
+    expect(component.nextUrl).toBe('next-url');
+    expect(component.previousUrl).toBe('previous-url');
     expect(component.loading).toBeFalse();
-    expect(swapiService.getCharacters).toHaveBeenCalledWith(
-      component.currentPage
-    );
   });
 
-  it('should load characters and set properties correctly', () => {
-    const mockResponse: CharacterResponse = {
+  it('should load the next page of characters', () => {
+    const mockResponse = {
       count: 1,
       next: 'next-url',
       previous: 'previous-url',
-      results: [
-        {
-          name: 'Luke Skywalker',
-          height: '172',
-          mass: '77',
-          hair_color: 'blond',
-          skin_color: 'fair',
-          eye_color: 'blue',
-          birth_year: '19BBY',
-          gender: 'male',
-          homeworld: 'Tatooine',
-          films: [],
-          species: [],
-          vehicles: [],
-          starships: [],
-          created: new Date(),
-          edited: new Date(),
-          url: '',
-        },
-      ],
+      results: [mockCharacter],
     };
     swapiService.getCharacters.and.returnValue(of(mockResponse));
 
-    component.loadCharacters();
-
-    expect(component.characterList).toEqual(mockResponse.results);
-    expect(component.nextUrl).toBe(mockResponse.next);
-    expect(component.previousUrl).toBe(mockResponse.previous);
-    expect(component.loading).toBeFalse();
-  });
-  it('should load the next page', () => {
-    spyOn(component, 'loadCharacters');
     component.nextUrl = 'next-url';
-
     component.loadNextPage();
 
     expect(component.currentPage).toBe(2);
-    expect(component.loadCharacters).toHaveBeenCalled();
+    expect(swapiService.getCharacters).toHaveBeenCalledWith(2);
   });
 
-  it('should not load the next page if nextUrl is null', () => {
-    spyOn(component, 'loadCharacters');
-    component.nextUrl = null;
+  it('should load the previous page of characters', () => {
+    const mockResponse = {
+      count: 1,
+      next: 'next-url',
+      previous: 'previous-url',
+      results: [mockCharacter],
+    };
+    swapiService.getCharacters.and.returnValue(of(mockResponse));
 
-    component.loadNextPage();
+    component.previousUrl = 'previous-url';
+    component.currentPage = 2;
+    component.loadPreviousPage();
 
     expect(component.currentPage).toBe(1);
-    expect(component.loadCharacters).not.toHaveBeenCalled();
+    expect(swapiService.getCharacters).toHaveBeenCalledWith(1);
   });
 
-  it('should add character to favorite', () => {
-    const character = { name: 'Luke Skywalker' } as StarWarsCharacter;
-    spyOn(Utils, 'get').and.returnValue('[]');
-    spyOn(Utils, 'set');
+  it('should check if a character is a favorite', () => {
+    favoriteService.isFavorite.and.returnValue(true);
 
-    component.addToFavorite(character);
+    expect(component.isFavorite(mockCharacter)).toBeTrue();
+    expect(favoriteService.isFavorite).toHaveBeenCalledWith(mockCharacter);
+  });
 
-    expect(Utils.set).toHaveBeenCalledWith(
-      'favorite',
-      JSON.stringify([character])
+  it('should add or remove a character from favorites', () => {
+    component.addToFavorite(mockCharacter);
+
+    expect(favoriteService.addOrRemoveFavorite).toHaveBeenCalledWith(
+      mockCharacter
     );
-  });
-
-  it('should not add duplicate character to favorite', () => {
-    const character = { name: 'Luke Skywalker' } as StarWarsCharacter;
-    spyOn(Utils, 'get').and.returnValue(JSON.stringify([character]));
-    spyOn(Utils, 'set');
-
-    component.addToFavorite(character);
-
-    expect(Utils.set).not.toHaveBeenCalled();
   });
 });
